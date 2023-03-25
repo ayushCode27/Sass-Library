989c30b6c0bafe762c40126290a58be76071f152
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-03-25 21:36:20 +0530

    purging unused css

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
-  return src('sigma/**/*.scss').pipe(sass()).pipe(dest('css'));
+  return src('sigma/**/*.scss')
+    .pipe(sass())
+    .pipe(
+      purgecss({
+        content: ['*.html'],
+      }),
+    )
+    .pipe(dest('css'));
 }
 
 function watchTask() {
-  watch(['sigma/**/*.scss'], buildStyles);
+  watch(['sigma/**/*.scss', '*.html'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
